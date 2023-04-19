e31fe7c11516b22f0a22df75c9901693bcc8caba
Joloharo <joeljoanlopez@gmail.com>
2023-04-19 17:13:38 +0200

    Ara si empezado

---
+++ practica1/Program.cs
@@ -0,0 +1,13 @@
+﻿using TCEngine;
+
+namespace TCGame
+{
+    public class App
+    {
+        public static void Main()
+        {
+            TecnoCampusEngine engine = new TecnoCampusEngine();
+            engine.Run(new InventoryGame());
+        }
+    }
+}
---
+++ practica1/Source/Engine/Game.cs
@@ -0,0 +1,12 @@
+﻿using SFML.Graphics;
+
+namespace TCEngine
+{
+    public interface Game
+    {
+        void Init(RenderWindow _window);
+        void DeInit();
+        void Update(float _dt);
+        void Draw(RenderWindow _window);
+    }
+}
