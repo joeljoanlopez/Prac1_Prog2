f8c24c69169ebba21d6118bd35f3e084b919f0eb
Joloharo <joeljoanlopez@gmail.com>
2023-05-01 19:27:03 +0200

    Revert "Arreglado"
    This reverts commit 473ef07921b224178e801918532a583a954574ea.

--- practica1/Source/Game/Item.cs
+++ practica1/Source/Game/Item.cs
@@ -5,8 +5,6 @@ namespace TCGame
 {
     public abstract class Item : Sprite
     {
-        public Item() {
-            this.Origin = new Vector2f(myTexture.Size.X/2, myTexture.Size.Y/2);
-        }
+
     }
 }
