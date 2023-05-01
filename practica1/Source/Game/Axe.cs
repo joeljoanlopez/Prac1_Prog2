d42e3efd02051d4de5f09efa3d29583d9455bdb7
Joloharo <joeljoanlopez@gmail.com>
2023-05-01 19:27:24 +0200

    Revert "Restart"
    This reverts commit 85f57c541602a064da719bdbed82ac773a3a7d2d.

---
+++ practica1/Source/Game/Axe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML;
+
+namespace TCGame
+{
+    internal class Axe : Item
+    {
+        //Private
+        private Sprite sprite;
+        private Texture texture;
+
+        public Axe() { 
+            texture = new Texture ("Data/Textures/Axe.png");
+            sprite = new Sprite (texture);
+        }
+    }
+}
