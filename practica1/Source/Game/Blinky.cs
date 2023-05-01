473ef07921b224178e801918532a583a954574ea
Joloharo <joeljoanlopez@gmail.com>
2023-05-01 19:04:04 +0200

    Arreglado

---
+++ practica1/Source/Game/Blinky.cs
@@ -0,0 +1,22 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCGame
+{
+    internal class Blinky : Item
+    {
+        //Private
+        private Sprite sprite;
+        private Texture texture;
+
+        public Blinky()
+        {
+            texture = new Texture("Data/Textures/Blinky.png");
+            sprite = new Sprite(texture);
+        }
+    }
+}
---
+++ practica1/Source/Game/Heart.cs
@@ -0,0 +1,22 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCGame
+{
+    internal class Heart : Item
+    {
+        //Private
+        private Sprite sprite;
+        private Texture texture;
+
+        public Heart()
+        {
+            texture = new Texture("Data/Textures/Heart.png");
+            sprite = new Sprite(texture);
+        }
+    }
+}
