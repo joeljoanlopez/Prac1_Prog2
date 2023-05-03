934d3017199e0f735737afda7ba4f20d5423c407
u1961605 <joeljoanlopez@gmail.com>
2023-05-03 13:23:23 +0200

    Update Item.cs

--- practica1/Source/Game/Item.cs
+++ practica1/Source/Game/Item.cs
@@ -5,6 +5,12 @@ namespace TCGame
 {
     public abstract class Item : Sprite
     {
+        private Sprite _Sprite;
+
+        Item()
+        {
+            _Sprite.Origin = new Vector2f(GetLocalBounds().Width, GetLocalBounds().Height) / 2;
+        }
 
     }
 }
